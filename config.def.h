831c7dc99cce26a400a0b4b2e76802a9f74ae7ff
frosty <passedgoandgot200@gmail.com>
2024-05-26 07:20:05 -0400

    patch: scratchpad

--- config.def.h
+++ config.def.h
@@ -60,6 +60,9 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -106,6 +109,8 @@ static const Key keys[] = {
 
 	{ MODKEY|ShiftMask,             XK_Delete, quit,           {0} }, /* quit */
         { MODKEY,                       XK_Delete, quit,           {1} }, /* restart */
+
+	{ MODKEY,                       XK_n,      togglescratch,  {.v = scratchpadcmd } },
 
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
